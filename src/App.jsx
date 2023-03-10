ba8459d63e29e865784c766784e28c3d4e5748af
ThandarSwe24 <thandarswe997@gmail.com>
2023-03-10 16:54:05 +0630

    api call

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import UseEffect from './UseEffect/UseEffect'
+
+const App = () => {
+  return (
+    <div>
+      <UseEffect/>
+    </div>
+  )
+}
+
+export default App
+
---
+++ src/UseEffect/UseEffect.jsx
@@ -0,0 +1,43 @@
+import React , {useState, useEffect}from 'react';
+import styled from 'styled-components';
+
+const Main = styled.div`
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center`;
+
+const H1 = styled.h1`
+        color:blue`;
+
+const H2 = styled.h2`
+        color:black`;
+
+
+const UseEffect = () => {
+
+    const [advice, setAdvice] = useState('');
+    
+    useEffect(() => {
+      const url = "https://api.adviceslip.com/advice";
+
+      const fetchData = async()=> {
+        const res = await fetch(url);
+        const data = await res.json();
+        setAdvice(data.slip.advice);
+      }
+      fetchData();
+    }, [])
+    
+  return (
+        <> 
+        <Main>
+        <H1>fetch api with useEffect</H1>
+        <H2>{advice}</H2>
+        </Main>
+      
+        </>
+  )
+}
+
+export default UseEffect
